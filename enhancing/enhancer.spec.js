682b1e5c89dd51987cee4f8c50775315e76f1593
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-13 16:43:32 -0400

    Created test for repair method for item

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,11 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('enhancer.js', () => {
+	describe('repair() method', () => {
+		it('restore items durability to 100', () => {
+			const items = { name: 'sword', durability: 100, enhancement: +1 };
+			expect(enhancer.repair(items)).toHaveProperty('durability', 100);
+		});
+	});
+});
