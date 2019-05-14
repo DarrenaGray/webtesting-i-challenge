9f2f430d78d16c8d7fc857354d6fc99bd664258f
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-14 10:28:17 -0400

    Added fix to repair function. Added conditional for success function

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,28 @@
 module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
+	succeed,
+	fail,
+	repair,
+	get
 };
 
 function succeed(item) {
-  return { ...item };
+	let { enhancement } = item;
+	if (enhancement < 20) {
+		enhancement += 1;
+	} else if ((enhancement = 20)) {
+		null;
+	}
+	return { ...item, enhancement };
 }
 
 function fail(item) {
-  return { ...item };
+	return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+	return { ...item, durability: 100 };
 }
 
 function get(item) {
-  return { ...item };
+	return { ...item };
 }
